81704a58642c4777f18efaf1f0aa616cb92dbc78
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2021-01-09 21:20:51 +0100

    imported the layout component and sass styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Layout from './components/layout/Layout';
+import './sass/styles.scss';
 
 function App() {
   return (
     <div className="App">
-    
+      <Layout />
     </div>
   );
 }
